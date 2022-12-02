770bdac3b8408cc449a78de7a0027012f6756e49
Thomas Smith <thsm@umich.edu>
2022-12-02 00:40:38 -0500

    Solve 2022 day 2

--- src/year2022.rs
+++ src/year2022.rs
@@ -1,13 +1,15 @@
 mod day1;
+mod day2;
 
 use std::io;
 
 use crate::part::Part;
 
 pub fn run(day: u8, part: Part, input_file: &str) -> io::Result<()> {
-    if day == 1 {
-        day1::run(part, input_file)
-    } else {
-        Err(io::Error::new(io::ErrorKind::Other, "Invalid day"))
-    }
+    let run_func = match day {
+        1 => day1::run,
+        2 => day2::run,
+        _ => Err(io::Error::new(io::ErrorKind::Other, "Invalid day"))?,
+    };
+    run_func(part, input_file)
 }
---
+++ src/year2022/day2.rs
@@ -0,0 +1,143 @@
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+
+use crate::part::Part;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Action {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Action {
+    fn score(self) -> u64 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+
+    fn dominator(self) -> Self {
+        match self {
+            Self::Rock => Self::Paper,
+            Self::Paper => Self::Scissors,
+            Self::Scissors => Self::Rock,
+        }
+    }
+
+    fn loser(self) -> Self {
+        match self {
+            Self::Rock => Self::Scissors,
+            Self::Paper => Self::Rock,
+            Self::Scissors => Self::Paper,
+        }
+    }
+
+    fn counter_to_get_outcome(self, outcome: Outcome) -> Self {
+        match outcome {
+            Outcome::Win => self.dominator(),
+            Outcome::Lose => self.loser(),
+            Outcome::Draw => self,
+        }
+    }
+
+    fn from_opponent_str(string: &str) -> Option<Self> {
+        match string {
+            "A" => Some(Self::Rock),
+            "B" => Some(Self::Paper),
+            "C" => Some(Self::Scissors),
+            _ => None,
+        }
+    }
+
+    fn from_your_str(string: &str) -> Option<Self> {
+        match string {
+            "X" => Some(Self::Rock),
+            "Y" => Some(Self::Paper),
+            "Z" => Some(Self::Scissors),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl Outcome {
+    fn from_match(you: Action, opponent: Action) -> Self {
+        if you == opponent {
+            Self::Draw
+        } else if you.dominator() == opponent {
+            Self::Lose
+        } else { // you.loser() == opponent
+            Self::Win
+        }
+    }
+
+    fn score(self) -> u64 {
+        match self {
+            Self::Win => 6,
+            Self::Draw => 3,
+            Self::Lose => 0,
+        }
+    }
+
+    fn from_str(string: &str) -> Option<Self> {
+        match string {
+            "X" => Some(Self::Lose),
+            "Y" => Some(Self::Draw),
+            "Z" => Some(Self::Win),
+            _ => None,
+        }
+    }
+}
+
+fn score(action: Action, outcome: Outcome) -> u64 {
+    action.score() + outcome.score()
+}
+
+pub fn run(part: Part, input_file: &str) -> io::Result<()> {
+    let file = File::open(input_file)?;
+    let reader = io::BufReader::new(file);
+
+    let mut cur_score: u64 = 0;
+    for line in reader.lines() {
+        let line = line?;
+        let words = line.split(' ').collect::<Vec<&str>>();
+        if words.len() != 2 {
+            Err(io::Error::new(io::ErrorKind::Other, "Invalid words"))?
+        }
+
+        let opponent = Action::from_opponent_str(words[0]).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "Invalid opponent action")
+        })?;
+
+        match part {
+            Part::Part1 => {
+                let you = Action::from_your_str(words[1]).ok_or_else(|| {
+                    io::Error::new(io::ErrorKind::Other, "Invalid action")
+                })?;
+                cur_score += score(you, Outcome::from_match(you, opponent));
+            },
+            Part::Part2 => {
+                let outcome = Outcome::from_str(words[1]).ok_or_else(|| {
+                    io::Error::new(io::ErrorKind::Other, "Invalid outcome")
+                })?;
+                cur_score += score(
+                    opponent.counter_to_get_outcome(outcome),
+                    outcome,
+                );
+            },
+        }
+    }
+    println!("{}", cur_score);
+
+    Ok(())
+}
