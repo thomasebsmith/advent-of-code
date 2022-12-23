eaf87f3fc13c04f602a07634685d83d8bfbfdde2
Thomas Smith <thsm@umich.edu>
2022-12-22 23:44:38 -0500

    TODO 2022 day 22 part 2 WIP

--- src/year2022/day22.rs
+++ src/year2022/day22.rs
@@ -91,104 +91,195 @@ struct Map {
 
 impl Map {
     pub fn new() -> Self {
-        Self { tiles: Vec::new(), my_position: None }
+        Self {
+            tiles: Vec::new(),
+            my_position: None,
+        }
+    }
+
+    pub fn print(&self) {
+        for (row_num, row) in self.tiles.iter().enumerate() {
+            for (col_num, tile) in row.iter().enumerate() {
+                let mut to_print = match tile {
+                    Tile::Nothing => ' ',
+                    Tile::Open => '.',
+                    Tile::Wall => '#',
+                };
+
+                if let Some(position) = self.my_position {
+                    if row_num == position.row && col_num == position.col {
+                        to_print = match position.direction {
+                            Direction::Left => '<',
+                            Direction::Right => '>',
+                            Direction::Down => 'v',
+                            Direction::Up => '^',
+                        };
+                    }
+                }
+
+                print!("{}", to_print);
+            }
+            println!();
+        }
     }
 
     pub fn add_row(&mut self, tiles: Vec<Tile>) {
         if self.my_position.is_none() {
-            self.my_position = tiles.iter().enumerate().filter(|(_, tile)| **tile == Tile::Open).map(|(col, _)| col).next().map(|col| Position { row: self.tiles.len(), col, direction: Direction::Right });
+            self.my_position = tiles
+                .iter()
+                .enumerate()
+                .filter(|(_, tile)| **tile == Tile::Open)
+                .map(|(col, _)| col)
+                .next()
+                .map(|col| Position {
+                    row: self.tiles.len(),
+                    col,
+                    direction: Direction::Right,
+                });
         }
         self.tiles.push(tiles);
+    }
+
+    fn try_move_to(&mut self, candidate_position: Position) {
+        let tile = self.tiles[candidate_position.row][candidate_position.col];
+        if tile == Tile::Open {
+            self.my_position = Some(candidate_position);
+        }
     }
 
     fn wrap_up(&mut self) {
         // TODO
         let position = self.my_position.as_mut().expect("Must have position");
 
-        let cur_row = position.row;
-        for (row_num, row) in (&self.tiles[position.row..]).iter().enumerate().map(|(i, row)| (i + cur_row, row)) {
-            if position.col >= row.len() {
-                break;
+        let candidate_position = if position.col < 50 {
+            // Wrap to left of C
+            Position {
+                row: 50 + position.col,
+                col: 50,
+                direction: Direction::Right,
             }
-            match row[position.col] {
-                Tile::Open => {
-                    position.row = row_num;
-                },
-                Tile::Wall => {
-                    position.row = cur_row;
-                },
-                Tile::Nothing => {
-                    break;
-                },
+        } else if position.col < 100 {
+            // Wrap to left of F
+            Position {
+                row: 100 + position.col,
+                col: 0,
+                direction: Direction::Right,
             }
-        }
+        } else {
+            // Wrap to bottom of F
+            Position {
+                row: 199,
+                col: position.col - 100,
+                direction: Direction::Up,
+            }
+        };
+
+        self.try_move_to(candidate_position);
     }
 
     fn wrap_down(&mut self) {
         // TODO
         let position = self.my_position.as_mut().expect("Must have position");
 
-        let cur_row = position.row;
-        for (row_num, row) in (&self.tiles[..position.row]).iter().enumerate().rev() {
-            if position.col >= row.len() {
-                break;
+        let candidate_position = if position.col < 50 {
+            // Wrap to top of B
+            Position {
+                row: 0,
+                col: 100 + position.col,
+                direction: Direction::Down,
             }
-            match row[position.col] {
-                Tile::Open => {
-                    position.row = row_num;
-                },
-                Tile::Wall => {
-                    position.row = cur_row;
-                },
-                Tile::Nothing => {
-                    break;
-                },
+        } else if position.col < 100 {
+            // Wrap to right of F
+            Position {
+                row: 100 + position.col,
+                col: 49,
+                direction: Direction::Left,
             }
-        }
+        } else {
+            // Wrap to right of C
+            Position {
+                row: position.col - 50,
+                col: 99,
+                direction: Direction::Left,
+            }
+        };
+
+        self.try_move_to(candidate_position);
     }
 
     fn wrap_left(&mut self) {
         // TODO
         let position = self.my_position.as_mut().expect("Must have position");
-        let row = &self.tiles[position.row];
 
-        let cur_col = position.col;
-        for (col, tile) in (&row[position.col..]).iter().enumerate().map(|(i, tile)| (i + cur_col, tile)) {
-            match tile {
-                Tile::Open => {
-                    position.col = col;
-                },
-                Tile::Wall => {
-                    position.col = cur_col;
-                },
-                Tile::Nothing => {
-                    break;
-                },
+        let candidate_position = if position.row < 50 {
+            // Wrap to left of D
+            Position {
+                row: 149 - position.row,
+                col: 0,
+                direction: Direction::Right,
             }
-        }
+        } else if position.row < 100 {
+            // Wrap to top of D
+            Position {
+                row: 100,
+                col: position.row - 50,
+                direction: Direction::Down,
+            }
+        } else if position.row < 150 {
+            // Wrap to left of A
+            Position {
+                row: 149 - position.row,
+                col: 50,
+                direction: Direction::Right,
+            }
+        } else {
+            // Wrap to top of A
+            Position {
+                row: 0,
+                col: position.row - 100,
+                direction: Direction::Down,
+            }
+        };
+
+        self.try_move_to(candidate_position);
     }
 
     fn wrap_right(&mut self) {
         // TODO
         let position = self.my_position.as_mut().expect("Must have position");
-        let row = &self.tiles[position.row];
 
-        let cur_col = position.col;
-        for (col, tile) in (&row[..position.col]).iter().enumerate().rev() {
-            match tile {
-                Tile::Open => {
-                    position.col = col;
-                },
-                Tile::Wall => {
-                    position.col = cur_col;
-                },
-                Tile::Nothing => {
-                    break;
-                },
+        let candidate_position = if position.row < 50 {
+            // Wrap to right of E
+            Position {
+                row: 149 - position.row,
+                col: 99,
+                direction: Direction::Left,
             }
-        }
-    }
+        } else if position.row < 100 {
+            // Wrap to bottom of B
+            Position {
+                row: 49,
+                col: 50 + position.row,
+                direction: Direction::Up,
+            }
+        } else if position.row < 150 {
+            // Wrap to right of B
+            Position {
+                row: 149 - position.row,
+                col: 149,
+                direction: Direction::Left,
+            }
+        } else {
+            // Wrap to bottom of E
+            Position {
+                row: 149,
+                col: position.row - 100,
+                direction: Direction::Up,
+            }
+        };
 
+        self.try_move_to(candidate_position);
+    }
 
     fn move_one(&mut self) {
         // TODO
@@ -205,8 +296,12 @@ impl Map {
                         Tile::Wall => {
                             // Do nothing
                         }
-                        Tile::Nothing => { self.wrap_left(); }
-                        Tile::Open => { position.col -= 1; }
+                        Tile::Nothing => {
+                            self.wrap_left();
+                        }
+                        Tile::Open => {
+                            position.col -= 1;
+                        }
                     }
                 }
             }
@@ -220,8 +315,12 @@ impl Map {
                         Tile::Wall => {
                             // Do nothing
                         }
-                        Tile::Nothing => { self.wrap_right(); }
-                        Tile::Open => { position.col += 1; }
+                        Tile::Nothing => {
+                            self.wrap_right();
+                        }
+                        Tile::Open => {
+                            position.col += 1;
+                        }
                     }
                 }
             }
@@ -238,8 +337,12 @@ impl Map {
                             Tile::Wall => {
                                 // Do nothing
                             }
-                            Tile::Nothing => { self.wrap_up(); }
-                            Tile::Open => { position.row -= 1; }
+                            Tile::Nothing => {
+                                self.wrap_up();
+                            }
+                            Tile::Open => {
+                                position.row -= 1;
+                            }
                         }
                     }
                 }
@@ -257,14 +360,17 @@ impl Map {
                             Tile::Wall => {
                                 // Do nothing
                             }
-                            Tile::Nothing => { self.wrap_down(); }
-                            Tile::Open => { position.row += 1; }
+                            Tile::Nothing => {
+                                self.wrap_down();
+                            }
+                            Tile::Open => {
+                                position.row += 1;
+                            }
                         }
                     }
                 }
             }
         }
-
     }
 
     pub fn perform(&mut self, instruction: Instruction) {
@@ -303,7 +409,11 @@ pub fn run<R: io::Read>(
 
         if !empty_line_seen {
             // Part of the map
-            let tiles = line.chars().map(Tile::new).collect::<Option<Vec<_>>>().ok_or_else(|| invalid_input("Invalid tile character"))?;
+            let tiles = line
+                .chars()
+                .map(Tile::new)
+                .collect::<Option<Vec<_>>>()
+                .ok_or_else(|| invalid_input("Invalid tile character"))?;
 
             map.add_row(tiles);
         } else {
@@ -314,26 +424,28 @@ pub fn run<R: io::Read>(
             let mut begin_index: usize = 0;
 
             for (i, ch) in line.chars().enumerate() {
-                let this_parse_state: u8 = if ch.is_ascii_digit() {
-                    1
-                } else {
-                    2
-                };
+                let this_parse_state: u8 =
+                    if ch.is_ascii_digit() { 1 } else { 2 };
 
                 if parse_state != 0 && parse_state != this_parse_state {
                     // Assume ASCII
                     let string = &line[begin_index..i];
                     match parse_state {
                         1 => {
-                            instructions.push(Instruction::Move(string.parse().map_err(invalid_input)?));
-                        },
+                            instructions.push(Instruction::Move(
+                                string.parse().map_err(invalid_input)?,
+                            ));
+                        }
                         2 => {
                             for ch in string.chars() {
-                                let direction = Direction::new(ch).ok_or_else(|| invalid_input("Unexpected direction"))?;
+                                let direction =
+                                    Direction::new(ch).ok_or_else(|| {
+                                        invalid_input("Unexpected direction")
+                                    })?;
                                 instructions.push(Instruction::Turn(direction));
                             }
-                        },
-                        _ => unreachable!()
+                        }
+                        _ => unreachable!(),
                     }
                     begin_index = i;
                 }
@@ -344,23 +456,33 @@ pub fn run<R: io::Read>(
             let string = &line[begin_index..];
             match parse_state {
                 1 => {
-                    instructions.push(Instruction::Move(string.parse().map_err(invalid_input)?));
-                },
+                    instructions.push(Instruction::Move(
+                        string.parse().map_err(invalid_input)?,
+                    ));
+                }
                 2 => {
                     for ch in string.chars() {
-                        let direction = Direction::new(ch).ok_or_else(|| invalid_input("Unexpected direction"))?;
+                        let direction =
+                            Direction::new(ch).ok_or_else(|| {
+                                invalid_input("Unexpected direction")
+                            })?;
                         instructions.push(Instruction::Turn(direction));
                     }
-                },
-                _ => unreachable!()
+                }
+                _ => unreachable!(),
             }
         }
     }
 
+    // TODO
     for instruction in instructions {
+        //map.print();
         map.perform(instruction);
+        //std::thread::sleep(std::time::Duration::from_millis(1000));
+        //println!("After {:?}     \tat {:?}", instruction, map.my_position.unwrap());
     }
 
+    //map.print();
     // TODO
     println!("{}", map.my_position.unwrap().password());
 
