5cdc6f597303d9c940abf329c61d4d4d730d58b7
Thomas Smith <thsm@umich.edu>
2025-12-04 00:37:43 -0500

    Solve 2025 day 4

--- src/year2025.rs
+++ src/year2025.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 use std::io;
 
@@ -16,6 +17,7 @@ pub fn run<R: io::Read>(
         1 => day01::run,
         2 => day02::run,
         3 => day03::run,
+        4 => day04::run,
         _ => Err(invalid_input("Invalid day"))?,
     };
 
